6daf96d4f7025ea0fdb2eda1786352a661d2302e
GITA <jgitanjali592@gmail.com>
2024-03-15 22:00:16 +0530

    this code initial to final GSAP

---
+++ GSAP-1/script.js
@@ -0,0 +1,20 @@
+// initial to final animation 
+
+// gsap.to("#box" , {
+//     x:1000 ,
+//     duration:2,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     delay:1
+// })
+
+// final to initial annimation
+
+// gsap.from("#box" , {
+//     x:1000 ,
+//     duration:2,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     delay:1
+// })
+
