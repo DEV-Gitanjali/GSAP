82cb2e59bcdde77572ea2733c19a848697edb1bb
GITA <jgitanjali592@gmail.com>
2024-03-15 22:33:37 +0530

    this code for gsap time line

--- GSAP-1/script.js
+++ GSAP-1/script.js
@@ -18,3 +18,32 @@
 //     delay:1
 // })
 
+// GSAP TimeLine
+var tl = gsap.timeline()
+
+tl.to("#box1" , {
+    x:1000,
+    rotate:360,
+    scale:0.5 ,
+    duration:2,
+    // delay:1
+
+})
+tl.to("#box2" , {
+    x:1000,
+    rotate:360,
+    scale:0.5 ,
+    duration:2,
+    // delay:1
+
+})
+
+
+tl.to("#box3" , {
+    x:1000,
+    rotate:360,
+    scale:0.5 ,
+    duration:2,
+    // delay:1
+
+})
