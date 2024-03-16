a657b78808eadbf220c4401a35b4c25eae32da97
GITA <jgitanjali592@gmail.com>
2024-03-16 13:28:59 +0530

    this code for gsap horizental scroll

---
+++ GSAP-Horizental Scroll/script.js
@@ -0,0 +1,13 @@
+gsap.to("#page2 h1" , {
+    transform:"translateX(-125%)",
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        scrub:"2",
+        markars:true ,
+        start:"top 0",
+        end:"top -100%",
+        pin:true
+
+    }
+})
