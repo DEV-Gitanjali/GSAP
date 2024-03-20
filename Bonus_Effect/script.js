020d07ea8978feb62bfc2a81fef3107a454bc0b6
GITA <jgitanjali592@gmail.com>
2024-03-20 22:31:17 +0530

    this code for bonus effect

--- Bonus_Effect/script.js
+++ Bonus_Effect/script.js
@@ -1,106 +1,43 @@
-//   var h1Text = document.querySelector("#firsth1") . textContent
+// var h1Text =document.querySelector("#firsth1").textContent
+// var splittedText = h1Text.split("")
 
-//  var splitedText =  h1Text.split(" ")
-
-//  console.log(splitedText)
-
-
-//   var clutter = " "
-//  splitedText.forEach(function(elem){
-//     clutter += `<span>${elem}</span>`
-
-//  })
-
-//  document.querySelector("#firsth1").innerHTML = clutter
-
-function locoscroll(){
-    gsap.registerPlugin(ScrollTrigger);
+// var clutter = ""
+// splittedText.forEach(function(elem) {
+//   clutter += `<span> ${elem}</span>`
+// })
+// document .querySelector("#firsth1").innerHTML = clutter
 
 
-    // --- SETUP START ---
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-    const locoScroll = new LocomotiveScroll({
-      el: document.querySelector("#main"),
-      smooth: true
-    });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
-    
-    // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy("#main", {
-      scrollTop(value) {
-        return arguments.length ? locoScroll.scrollTo(value, {duration: 0, disableLerp: true}) : locoScroll.scroll.instance.scroll.y;
-      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-      getBoundingClientRect() {
-        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-      },
-      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-    });
-    
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-    ScrollTrigger.defaults({ scroller: "#main" });
-    // --- SETUP END ---
-    
-    
-   
-    
-    
-    
-    
-    
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-    ScrollTrigger.refresh();
-    
-
-
-}
 
 
 function textSpliting(){
-    
-    
-    var allH1 = document.querySelectorAll("#page2 h1")
-    allH1.foreach(function ( elem){
-        
-        var clutter =" "
-        var h1Text = elem.textContent
-        
-        var splitedText =  h1text.split(" ")
-        
-        splitedText.foreach(function(e){
-            clutter += `<span>${e}</span>`
-            
-        })
-        
-        e.innerHTML = clutter
-        
-        
+  var allH1 = document.querySelectorAll("#page2 h1")
+  allH1.forEach(function(elem){
+    var clutter = " " 
+    var h1Text = elem.textContent
+    var splittedText = h1Text.split(" ")
+    splittedText.forEach(function(e){
+      clutter += ` <span>${e}</span>`
     })
+    elem.innerHTML = clutter
+  })
 }
-
 
 function gsapAnimation(){
-
+  gsap.to("#page2 h1 span",{
+    color: "white",
+    stagger: 0.1,
+    scrollTrigger:{
+      trigger:"#page2 h1",
+      scroller:"body",
+      markers:true,
+      start:"top 50%",
+      end:"top -10%",
+      scrub: 2 
+    }
     
-    gsap.to("#page2 h1 span ",{
-        color:"#E3E3C4",
-        stagger:0.1,
-        scrollTrigger:{
-            trigger:"#page2 h1",
-            scrooler: "#main",
-            marker:true,
-            start:"top 50%",
-            end:"top 10%",
-            scrub:1,
-            
-        }
-        
-        
-    })
+  })
 }
 
-locoScroll();
- textSpliting();
- gsapAnimation();
+textSpliting()
+gsapAnimation()
